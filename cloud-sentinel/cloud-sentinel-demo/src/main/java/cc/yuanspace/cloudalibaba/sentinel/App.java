5b0b14d6082eab649d578d8eb1acf8e3b76db49d
originyuan <1498202295@qq.com>
2022-07-04 23:08:57 +0800

    Sentinel 集成

---
+++ cloud-sentinel/cloud-sentinel-demo/src/main/java/cc/yuanspace/cloudalibaba/sentinel/App.java
@@ -0,0 +1,17 @@
+package cc.yuanspace.cloudalibaba.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 开启服务注册与发现功能
+@EnableFeignClients  // 开启OpenFeign
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+        System.out.println("**** 启动成功 ****");
+    }
+}
