b9b889f04e741d69a11abcf34776e2ef5289c5a7
originyuan <1498202295@qq.com>
2022-07-05 18:09:24 +0800

    调整目录

---
+++ cloud-sentinel/cloud-sentinel-demo/src/main/java/cc/yuanspace/cloudalibaba/sentinel/controller/TestController.java
@@ -0,0 +1,14 @@
+package cc.yuanspace.cloudalibaba.sentinel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("")
+    public String get() {
+        return "123";
+    }
+}
