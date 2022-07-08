2d6d6f470f78e441f33f6cc57f1c43f31c11ecd1
originyuan <1498202295@qq.com>
2022-07-08 23:45:21 +0800

    gateway过滤器基本使用

---
+++ cloud-gateway/cloud-gw-filters/src/main/java/cc/yuanspace/cloudalibaba/gateway/predicates/App.java
@@ -0,0 +1,15 @@
+package cc.yuanspace.cloudalibaba.gateway.predicates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 开启服务注册与发现功能
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+        System.out.println("**** 启动成功 ****");
+    }
+}
--- cloud-gateway/cloud-gw-server/src/main/java/cc/yuanspace/cloudalibaba/gateway/server/controller/TestController.java
+++ cloud-gateway/cloud-gw-server/src/main/java/cc/yuanspace/cloudalibaba/gateway/server/controller/TestController.java
@@ -1,6 +1,7 @@
 package cc.yuanspace.cloudalibaba.gateway.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,11 @@ public class TestController {
     @GetMapping("/get")
     public String get() {
         return "gw server get";
+    }
+
+
+    @GetMapping("/getHeader")
+    public String getHeader(@RequestHeader("token") String token) {
+        return "getHeader " + token;
     }
 }
